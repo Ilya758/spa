60eb40219f6e99396188a24f56dd120a6ece59bb
Illia Skaryna <subscriber6436@gmail.com>
2023-07-30 12:02:53 +0300

    feat: added redirect to home page, graphql, apollo

--- next.config.js
+++ next.config.js
@@ -20,6 +20,13 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  redirects: () => [
+    {
+      source: '/',
+      destination: '/home',
+      permanent: true,
+    },
+  ],
 };
 
 module.exports = nextConfig;
