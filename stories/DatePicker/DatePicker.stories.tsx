faba7377f768a3139a5ff1aee0ee4f3b3fd8df29
Illia Skaryna <subscriber6436@gmail.com>
2023-08-05 15:21:03 +0300

    fix: minor edit

--- stories/DatePicker/DatePicker.stories.tsx
+++ stories/DatePicker/DatePicker.stories.tsx
@@ -11,9 +11,5 @@ export default meta;
 type Story = StoryObj<typeof DatePicker>;
 
 export const Primary: Story = {
-  args: {
-    href: '',
-    color: 'bronzed',
-    children: 'Text in link',
-  },
+  args: {},
 };
